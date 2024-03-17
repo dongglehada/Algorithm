651b81b130aa14e2159573cadd2d978d0f1252ae
dongglehada <ghddns34@gmail.com>
2024-03-17 20:47:58 +0900

    약수의 합

--- Programmers/Level1/약수의 합.playground/Contents.swift
+++ Programmers/Level1/약수의 합.playground/Contents.swift
@@ -1,5 +1,10 @@
 func solution(_ n:Int) -> Int {
     var resultAry: [Int] = []
+    
+    if n == 0 {
+        return 0
+    }
+    
     for i in (1...n) {
         if n % i == 0 {
             resultAry.append(i)
